6ab0cb5acc58abce8aee115ae1769b7a1a513c3b
Morgan Shan <mshan0290@gmail.com>
2019-04-09 12:17:20 -0700

    testing another i/o

--- src/com/examples/javaprog/Palindrome.java
+++ src/com/examples/javaprog/Palindrome.java
@@ -1,12 +1,19 @@
 package com.examples.javaprog;
 
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Scanner;
+import java.io.BufferedReader;
 
 public class Palindrome {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.print("Enter any word: ");
-        String word = s.next();
+//        String word = s.next();
+        String word = r.readLine();
+
         int len = word.length();
         String first_half = word.substring(0, len / 2);
         String last_half = "";
