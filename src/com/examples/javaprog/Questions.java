1c3a08c75ac1def68451f3959872dc1ee9bbfd04
Morgan Shan <mshan0290@gmail.com>
2019-04-19 12:20:05 -0700

    gui testing

--- src/com/examples/javaprog/Questions.java
+++ src/com/examples/javaprog/Questions.java
@@ -1,27 +1,36 @@
 package com.examples.javaprog;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class Questions implements ActionListener {
+    private JButton button;
+    private JTextField text_field;
+
     public static void main(String[] args) {
         Questions q = new Questions();
         q.start();
     }
 
-    public void start() {
-        JFrame frame = new JFrame();               // creates a frame
-        button = new JButton(":)");   // creates a button with words in passed in arg
-        button.addActionListener(this);         // adds this object button to list of listeners
+    private void start() {
+        JFrame frame = new JFrame();
+        button = new JButton("Type \"apple\" then click me")
+        button.addActionListener(this);  // adds this object button to list of listeners
+
+        text_field = new JTextField();
 
 
-        frame.getContentPane().add(button);                   // puts the button onto the frame's content pane
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // quits program after clicking x on window
-        frame.setSize(250, 250);                // size of frame (x,y)
-        frame.setVisible(true);                               // makes frame visible
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.NORTH, text_field);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent Event) {
-        button.setText("REE");
+        if (text_field.getText().equalsIgnoreCase("apple")) {  // if the text box has "apple" written in
+            button.setText("Good job.");  // make the button say "good job"
+        }
     }
 }
