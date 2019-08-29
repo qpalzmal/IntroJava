0b7f667aed92b66b9344b53ffaedfcb19a466316
qpalzmal <mshan0290@gmail.com>
2019-08-28 22:35:06 -0700

    comments to clarify what's happening

--- src/com/examples/javaprog/Palindrome.java
+++ src/com/examples/javaprog/Palindrome.java
@@ -14,9 +14,9 @@ public class Palindrome {
         String last_half = "";
 
         for (int i = 0; i < len / 2; i++) {
-            last_half += ((word.charAt(len - (i + 1))));
+            last_half += ((word.charAt(len - (i + 1))));	// gets the characters from the end of the word and stops once it reaches the middle of the word
         }
-        if (first_half.equalsIgnoreCase(last_half)) {
+        if (first_half.equalsIgnoreCase(last_half)) {	// compares the first half with the second half
             System.out.println("Your word is a palindrome");
         } else {
             System.out.println("Your word isn't a palindrome");
